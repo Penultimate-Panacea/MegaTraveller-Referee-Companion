e2b810d5942d9da96d191e6612a7af9c0231f46a
Rian Fantozzi <rian+github@fantozzi.org>
2020-02-11 23:37:42 -0500

    Added Boilerplate Test for DiceRoller

---
+++ test_diceRoller.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestDiceRoller(TestCase):
+    def test_roll_1d6(self):
+        self.fail()
+
+    def test_roll_1d3(self):
+        self.fail()
+
+    def test_roll_2d6(self):
+        self.fail()
+
+    def test_roll_nd6(self):
+        self.fail()
+
+    def test_roll_d66(self):
+        self.fail()
