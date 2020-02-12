6439cfc7ace2d3ccd17f9bfe242ef2e8620bab10
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-12 11:58:56 -0500

    Split World into mutliple types

---
+++ code/WorldTypes.py
@@ -0,0 +1,44 @@
+class Planet:
+    def __init__(self):
+        self.size_details = None
+        self.atmo_details = None
+        self.hydr_details = None
+        self.pops_details = None
+        self.govt_details = None
+        self.lawl_details = None
+        self.name = None
+        self.sector = None
+        self.coord = None
+        self.orbit = None
+        self.star = None
+
+
+class GasGiant:
+    def __init__(self):
+        self.size_details = None
+        self.atmo_details = None
+        self.hydr_details = None
+        self.pops_details = None
+        self.govt_details = None
+        self.lawl_details = None
+        self.name = None
+        self.sector = None
+        self.coord = None
+        self.orbit = None
+        self.star = None
+
+
+class Planetoid:
+    def __init__(self):
+        self.size_details = None
+        self.atmo_details = None
+        self.hydr_details = None
+        self.pops_details = None
+        self.govt_details = None
+        self.lawl_details = None
+        self.name = None
+        self.sector = None
+        self.coord = None
+        self.orbit = None
+        self.star = None
+
