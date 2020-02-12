d4e0909f2a5fd140f1630564394a81a0359c5f41
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-11 19:59:26 -0500

    Polished DiceRoller and added nD6 roller

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,21 +1,34 @@
 import random
-class DiceRoller(seed):
-    def rollD6():
+class DiceRoller:
+    def __init__(self, uniqueSeed):
+        random.seed(uniqueSeed, 2)
+
+    def roll1D6(self):
         roll = random.randint(1,6)
         return roll
 
-    def rollD3():
+    def rollD3(self):
         roll =random.randint(1,6) % 3
         return roll
 
-    def roll2D6():
-        result1 = rollD6()
-        result2 = rollD6()
+    def roll2D6(self):
+        result1 = self.roll1D6()
+        result2 = self.roll1D6()
         roll = result1 + result2
         return roll
 
-    def rollD66():
+    def rollND6(self, n):
+        i = 0
+        roll = 0
+        while n > i:
+            roll += self.roll1D6()
+            i += 1
+        return roll
+
+    def rollD66(self):
         result1 = random.randint(1,6)
         result2 = random.randint(1,6)
         roll = [result1, result2]
         return roll
+
+
