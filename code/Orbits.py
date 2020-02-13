e51271726d8002280142bc41854ddda3740ce8e5
Rian Fantozzi <rian+github@fantozzi.org>
2020-02-12 22:51:06 -0500

    Added Orbit Class

---
+++ code/Orbits.py
@@ -0,0 +1,10 @@
+class OrbitTable:
+
+class Orbit:
+    def __init__(self):
+        self.number = None
+        self.is_inner = False
+        self.is_hab = False
+        self.is_outer = False
+        self.is_incinerated = False
+        self.body = None
