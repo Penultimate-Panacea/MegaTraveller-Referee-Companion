b676bbb0b6fb02c3ccf732f0cd2fc108867b4943
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-15 14:24:49 -0500

    Continued to work on temp_gen

--- code/AtmoDetailGenerator.py
+++ code/AtmoDetailGenerator.py
@@ -2,10 +2,11 @@ import diceroller
 
 
 class AtmoDetailGenerator:
-    def __init__(self, planet, seed, orbit, star):
+    def __init__(self, planet, seed, orbit, star, orbit_zone):
         self.planet = planet
         self.dice = diceroller(seed)
         self.orbit = orbit #  just the number
+        self.orbit_zone = orbit_zone
         self.star = star
         self.composition = None
         self.pressure = None
@@ -87,7 +88,7 @@ class AtmoDetailGenerator:
             print("Something has broken in the pressure generator")
         return
 
-    def generate_surface_pressure(self):
+    def calculate_surface_temperature(self):
         Ia_schedule = [27.36, 21.25, 18.09, 16.87, 15.75, 15.03, 16.09, 17.27, 17.65, 18.09, 18.49, 18.95, 19.38]
         Ib_schedule = [22.80, 14.70, 11.07, 10.40, 09.27, 08.45, 08.84, 09.49, 10.40, 11.95, 14.65, 17.27, 18.49]
         II_schedule = [20.31, 11.68, 06.85, 05.40, 04.95, 04.75, 04.86, 05.22, 05.46, 07.04, 08.24, 11.05, 11.28]
@@ -95,5 +96,28 @@ class AtmoDetailGenerator:
         IV_schedule = [16.87, 06.69, 03.53, 02.47, 02.09, 01.86, 01.60, 01.49, 01.47, 99999, 99999, 99999, 99999]
         #  Final four values above are placeholder for non existent value, set ludicrously large for debugging
         V_schedule = [15.38, 06.12, 03.08, 02.00, 01.69, 01.37, 01.05, 00.90, 00.81, 00.53, 00.45, 00.29, 00.18]
-        VI_schedule = [15.38, 06.12, 03.08, 02.00, 01.69, 01.37, 01.05, 00.90, 00.81, 00.53, 00.45, 00.29, 00.18]
+        VI_schedule = [99999, 99999, 99999, 99999, 99999, 00.99, 00.75, 00.66, 00.58, 00.40, 00.32, 00.21, 00.09]
         #  First five values above are placeholder for non existent value, set ludicrously large for debugging
+        D_schedule = [0.46, 0.27, 0.13, 0.09, 0.08, 0.07]
+        orbit_factor = [836.345, 591.385, 447.045, 274.025, 295.693, 223.523, 164.021, 118.277, 84.484, 60.046, 42.569,
+                        30.140, 21.326, 15.085, 10.668, 7.544, 5.335, 3.772, 2.667, 1.886]
+        asorb_hab_thin = [0.900, 0.829, 0.803, 0.811, 0.782, 0.789, 0.795, 0.802, 0.808, 0.814, 0.818]
+        asorb_nohab_thin = [0.800, 0.744, 0.736, 0.752, 0.738, 0.753, 0.767, 0.782, 0.796, 0.810, 0.818]
+        asorb_hab_stdense = [0.900, 0.900, 0.860, 0.860, 0.820, 0.780, 0.740, 0.700, 0.660, 0.620, 0.618]
+        asorb_nohab_stdense = [0.800, 0.811, 0.789, 0.799, 0.774, 0.747, 0.718, 0.687, 0.654, 0.619, 0.619]
+        asorb_hab_special = [0.740, 0.697, 0.672, 0.676, 0.648, 0.613, 0.577, 0.539, 0.500, 0.500, 0.500]
+        asorb_nohab_special = [0.680, 0.646, 0.635, 0.644, 0.625, 0.599, 0.570, 0.537, 0.500, 0.500, 0.500]
+        asorb_hab_E = [0.900, 0.900, 0.882, 0.883, 0.866, 0.850, 0.836, 0.821, 0.807, 0.793, 0.773]
+        asorb_nohab_E = [0.800, 0.811, 0.807, 0.817, 0.813, 0.809, 0.805, 0.800, 0.794, 0.787, 0.773]
+        gg_effect = [1.00, 1.00, 1.00, 1.00, 1.05, 1.05, 1.10, 1.10, 1.15, 1.15, self.gg_generate(), self.gg_generate(),
+                     self.gg_generate(), 1.15, 1.10, 1.00]
+
+
+    def gg_generate(self):
+        a = [1.2, 1.2, 1.3, 1.3, 1.4, 1.4, 1.5, 1.5, 1.6, 1.6, 1.7]
+        b = [1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2]
+        gg_roll = self.dice.roll2d6()
+        if self.pressure.uwp[2] == 10:
+            return a[gg_roll]
+        elif self.planet.uwp[2] == 10 or 11:
+            return  b[gg_roll]
