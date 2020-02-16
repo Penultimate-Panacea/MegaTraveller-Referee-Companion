db1ececae3c6673b03fbe6134c8402bd72ed4292
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-16 18:52:52 -0500

    Added aggressive values

--- code/SocialOutlook.py
+++ code/SocialOutlook.py
@@ -7,8 +7,8 @@ class SocialOutlook:
         self.dice = diceroller(seed)
         self.progressive_attitude = self.generate_progressive_attitude()
         self.progressive_action = self.generate_progressive_action()
-        self.aggressive_attitude = None
-        self.aggressive_action = None
+        self.aggressive_attitude = self.generate_aggressive_attitude()
+        self.aggressive_action = self.generate_aggresive_action()
         self.extensiveness_global = None
         self.extensiveness_intstl = None
 
@@ -47,3 +47,37 @@ class SocialOutlook:
         else:
             index = roll
         return prog_actions[index - 2]
+
+    def generate_aggressive_attitude(self):
+        agg_attitute = ["Expansionistic", "Expansionistic", "Competitve", "Competetive", "Competetive", "Unaggressive",
+                        "Unaggressive", "Unaggressive", "Unaggressive", "Passive", "Passive", "Passive"]
+        dice_mods = 0
+        if self.planet.uwp[6] >= 10:
+            dice_mods += 1
+        roll = self.dice.roll2d6()
+        roll += dice_mods
+        if roll > 13:
+            index = 13
+        else:
+            index = roll
+        return agg_attitute[index - 2]
+
+    def generate_aggresive_action(self):
+        agg_action = ["Militant", "Militant", "Militant", "Neutral", "Neutral", "Neutral", "Neutral", "Peaceable",
+                      "Peaceable", "Peaceable", "Conciliatory", "Conciliatory"]
+        dice_mods = 0
+        if self.aggressive_attitude == "Expansionistic":
+            dice_mods -= 2
+        elif self.aggressive_attitude == "Competitive":
+            dice_mods -= 1
+        elif self.aggressive_attitude == "Passive":
+            dice_mods += 2
+        if self.planet.uwp[6] >= 10:
+            dice_mods += 1
+        roll = self.dice.roll2d6()
+        roll += dice_mods
+        if roll > 13:
+            index = 13
+        else:
+            index = roll
+        return  agg_action[index - 2]
