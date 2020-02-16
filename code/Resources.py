fbd82758b662d516359279a8d8181b147d97e44a
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-16 15:44:34 -0500

    Initial Resources Class __init__ def

--- code/Resources.py
+++ code/Resources.py
@@ -1,2 +1,13 @@
+import diceroller
+
+
 class Resources:
-# TODO All of it
+    def __init__(self, planet, seed, orbit, star, orbit_zone):
+        self.planet = planet
+        self.dice = diceroller(seed)
+        self.natural = None
+        self.processed = None
+        self.manufactured = None
+        self.information = None
+
+
