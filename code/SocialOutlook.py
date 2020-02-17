d70003bb4e4444bb3067cdf2a14d0d8abe804b49
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-16 20:42:17 -0500

    Added Extensiveness Values

--- code/SocialOutlook.py
+++ code/SocialOutlook.py
@@ -9,8 +9,8 @@ class SocialOutlook:
         self.progressive_action = self.generate_progressive_action()
         self.aggressive_attitude = self.generate_aggressive_attitude()
         self.aggressive_action = self.generate_aggresive_action()
-        self.extensiveness_global = None
-        self.extensiveness_intstl = None
+        self.extensiveness_globe = self.generate_extensiveness_globe()
+        self.extensiveness_intstl = self.generate_extensiveness_intstl()
 
     def generate_progressive_attitude(self):
         prog_attitudes = ["Radical", "Radical", "Progressive", "Progressive", "Progressive", "Progressive",
@@ -81,3 +81,59 @@ class SocialOutlook:
         else:
             index = roll
         return  agg_action[index - 2]
+
+    def generate_extensiveness_globe(self):
+        globe = ["Monolithic", "Monolithic", "Harmonious", "Harmonious", "Harmonious", "Harmonious", "Discordant",
+                 "Discordant", "Discordant", "Discordant", "Fragmented", "Fragmented"]
+        dice_mods = 0
+        if self.planet.uwp[5] < 3:
+            dice_mods += 1
+        elif self.planet.uwp[5] == 7:
+            dice_mods += 4
+        elif self.planet.uwp[5] == 15:
+            dice_mods -= 1
+        if self.aggressive_attitude == "Passive":
+            dice_mods += 2
+        if self.planet.uwp[6] < 5:
+            dice_mods += 1
+        elif self.planet.uwp[6] > 9:
+            dice_mods -= 1
+        roll = self.dice.roll2d6()
+        roll += dice_mods
+        if roll > 13:
+            index = 13
+        elif roll < 2:
+            index = 2
+        else:
+            index = roll
+        return globe[index -2]
+
+    def generate_extensiveness_intstl(self):
+        interstellar = ["Xenophillic", "Xenophillic", "Friendly", "Friendly", "Friendly", "Friendly", "Aloof", "Aloof",
+                        "Aloof", "Aloof", "Xenophobic", "Xenophobic"]
+        dice_mods = 0
+        if self.planet.uwp[0] == 'A':
+            dice_mods -= 2
+        elif self.planet.uwp[0] == 'B':
+            dice_mods -= 1
+        elif self.planet.uwp[0] == 'D':
+            dice_mods += 1
+        elif self.planet.uwp[0] == 'E':
+            dice_mods += 2
+        elif self.planet.uwp[0] == 'X':
+            dice_mods += 3
+        if self.progressive_attitude == "Conservative":
+            dice_mods += 2
+        elif self.progressive_attitude == "Reactionary":
+            dice_mods += 4
+        if self.planet.uwp[6] > 9:
+            dice_mods += 1
+        roll = self.dice.roll2d6()
+        roll += dice_mods
+        if roll > 13:
+            index = 13
+        elif roll < 2:
+            index = 2
+        else:
+            index = roll
+        return interstellar[index - 2]
