e98774c066e48a58e55b028e2914ada3096cfc1c
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-17 16:09:46 -0500

    Finished representative authority determination function

--- code/GovDetailGenerator.py
+++ code/GovDetailGenerator.py
@@ -14,4 +14,61 @@ class GovernmentDetails:
         org_roll = self.dice.roll2d6()
         return orgs[org_roll - 2]
 
+    def handle_balkanized(self):
+        # TODO THIS
+        return
+
+    def determine_respresentative_authority(self):
+        if self.planet.uwp[5]  == 0:
+            return "No authority"
+        elif self.planet.uwp[5] == 1:
+            return self.generate_representative_authority()
+        elif self.planet.uwp[5] == 2:
+            return "Democracy"
+        elif self.planet.uwp[5] == 3:
+            roll = self.dice.roll1d6()
+            if roll < 5:
+                return "Elite Council"
+            else:
+                return "Several Councils"
+        elif 3 < self.planet.uwp[5] < 7:
+            return self.generate_representative_authority()
+        elif self.planet.uwp[5] == 7:
+            return self.handle_balkanized()
+        elif self.planet.uwp[5] == 8 or 9:
+            return "Several Councils"
+        elif self.planet.uwp[5] == 10:
+            roll = self.dice.roll1d6()
+            if roll < 6:
+                return "Ruler"
+            else:
+                return "Elite Council"
+        elif self.planet.uwp[5] == 11:
+            roll = self.dice.roll1d6()
+            if roll < 6:
+                return "Ruler"
+            else:
+                return "Elite Council"
+        elif self.planet.uwp[5] == 12:
+            roll = self.dice.roll1d6()
+            if roll < 5:
+                return "Elite Council"
+            else:
+                return "Several Councils"
+        elif self.planet.uwp[5] == 13:
+            result = self.generate_representative_authority()
+            while result == "Democracy":
+                result = self.generate_representative_authority()
+            return result
+        elif self.planet.uwp[5] == 14
+            result = self.generate_representative_authority()
+            while result == "Democracy":
+                result = self.generate_representative_authority()
+            return result
+        elif self.planet.uwp[5] == 15:
+            roll = self.dice.roll1d6()
+            if roll < 5:
+                return "Elite Council"
+            else:
+                return "Several Councils"
 
