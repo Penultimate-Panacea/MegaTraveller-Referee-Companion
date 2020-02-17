3c316b990868859ede5d8f6e8d4437fbe84d4323
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-17 10:45:01 -0500

    Added eating habits

--- code/Customs.py
+++ code/Customs.py
@@ -67,3 +67,54 @@ class Customs:
                              "Tatooing on body for " + self.generate_practicing_group(),
                              "Tatooing on body for " + self.generate_practicing_group(),
                              "Hidden tatooing for " + self.generate_practicing_group()]
+        dressing_habits_lol = [dressing_habits_0, dressing_habits_1, dressing_habits_2, dressing_habits_3,
+                               dressing_habits_4, dressing_habits_5]
+        roll1 = self.dice.roll_1d6() - 1
+        roll2 = self.dice.roll_1d6() - 1
+        chosen_dressing_habit = dressing_habits_lol[roll1][roll2]
+        return chosen_dressing_habit
+
+    def generate_eating_habits(self):
+        eating_habits_0 = ["Unusual foods for " + self.generate_practicing_group(),
+                           "Unusual foods for " + self.generate_practicing_group(),
+                           "Unusual beverages for " + self.generate_practicing_group(),
+                           "Unusual beverages for " + self.generate_practicing_group(),
+                           "Unusual food preparation for " + self.generate_practicing_group(),
+                           "Unusual food preparation for " + self.generate_practicing_group()]
+        eating_habits_1 = [self.generate_practicing_group() + " are segregated during meals",
+                           self.generate_practicing_group() + " are segregated during meals",
+                           self.generate_practicing_group() + " are vegetarians",
+                           self.generate_practicing_group() + " are vegetarians",
+                           self.generate_practicing_group() + " are carnivorous",
+                           self.generate_practicing_group() + " are carnivorous"]
+        eating_habits_2 = [self.generate_practicing_group() + " are omnivorous",
+                           self.generate_practicing_group() + " are omnivorous",
+                           "Certain colored food taboo for " + self.generate_practicing_group(),
+                           "Certain colored food taboo for " + self.generate_practicing_group(),
+                           "Certain shaped food taboo for " + self.generate_practicing_group(),
+                           "Certain food sources taboo for " + self.generate_practicing_group()]
+        eating_habits_3 = [self.generate_practicing_group() + " eats in special location",
+                           self.generate_practicing_group() + " eats only in private",
+                           self.generate_practicing_group() + " eats in a special orientation",
+                           self.generate_practicing_group() + " eats with unusual utensils",
+                           self.generate_practicing_group() + " only eats at home",
+                           self.generate_practicing_group() + " only eats at home"]
+        eating_habits_4 = [self.generate_practicing_group() + " eat at unusual times",
+                           self.generate_practicing_group() + " eat at unusual times",
+                           self.generate_practicing_group() + " eat only certain times",
+                           self.generate_practicing_group() + " eat only certain ways",
+                           "Rituals before eating for " + self.generate_practicing_group(),
+                           "Rituals after eating for " + self.generate_practicing_group()]
+        eating_habits_5 = ["One sex eats the other's leftovers", 
+                           "One age eats the other's leftovers",
+                           self.generate_practicing_group() + " eats the " + self.generate_practicing_group() +
+                           " leftovers",
+                           "One class eats the other's leftovers",
+                           "One race eats the others leftovers",
+                           self.generate_practicing_group() + " are cannibalistic"]
+        eating_habits_lol = [eating_habits_0, eating_habits_1, eating_habits_2, eating_habits_3, eating_habits_4,
+                             eating_habits_5]
+        roll1 = self.dice.roll_1d6() - 1
+        roll2 = self.dice.roll_1d6() - 1
+        chosen_eating_habit = eating_habits_lol[roll1][roll2]
+        return chosen_eating_habit
