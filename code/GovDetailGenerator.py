c128d668078544c087a913fe6f5ed15411798f9d
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-17 16:09:15 -0500

    Added representative authority generation

--- code/GovDetailGenerator.py
+++ code/GovDetailGenerator.py
@@ -8,4 +8,10 @@ class GovernmentDetails:
         self.representative_authority = self.determine_respresentative_authority()
         self.other_authorities = None
 
+    def generate_representative_authority(self):
+        orgs = ["Democracy", "Elite Council", "Elite Council", "Elite Council", "Ruler", "Ruler", "Several Councils",
+                "Several Councils", "Several Councils", "Several Councils", "Democracy"]
+        org_roll = self.dice.roll2d6()
+        return orgs[org_roll - 2]
+
 
