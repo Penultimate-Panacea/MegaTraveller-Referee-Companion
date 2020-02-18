d6c33705c7f839dc01a1857592aa0d148edc13d6
Penultimate-Panacea <rian+github@fantozzi.org>
2020-02-17 21:08:47 -0500

    Added Religion class

---
+++ code/Religion.py
@@ -0,0 +1,12 @@
+class Religion:
+    def __init__(self, planet, seed):
+        self.planet = planet
+        self.dice = diceroller(seed)
+        self.god_view = None
+        self.spiritual_view = None
+        self.devotion = None
+        self.organization = None
+        self.formality = None
+        self.fervor = None
+        self.adherents = None
+        self.profile = None  # Maybe this should be a toString style function?
